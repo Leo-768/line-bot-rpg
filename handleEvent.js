6bcd774d921d389f3d443eddb5f36e13b1cebea4
Leo-768 <66005818+Leo-768@users.noreply.github.com>
2022-06-09 14:19:39 +0800

    Update handleEvent.js

--- handleEvent.js
+++ handleEvent.js
@@ -17,18 +17,23 @@ async function handleEvent(event//:import('@line/bot-sdk').WebhookEvent
     }
 
     if (event.type === 'postback') {
-        if (event.postback.data.startsWith('ui'))
+        if (event.postback.data.startsWith('ui')){
             if (event.postback.data === 'ui-start') {
                 await dataSetUp(event.source.userId)
                 client.linkRichMenuToUser(event.source.userId, richMenuAliasToId['next'])
                 memory[event.source.userId] = { stage: 'begin', stage2: 0, stage3: 0 }
                 ref.child(event.source.userId).update(memory[event.source.userId])
-                run(event.source.userId, event.replyToken)
+                return run(event.source.userId, event.replyToken)
                 //client.replyMessage(event.replyToken, { type: 'text', text: '...' })//message(event.source.userId))
-            } else if (event.postback.data === 'ui-next') {
-                run(event.source.userId, event.replyToken)
+            } else if (event.postback.data === 'ui-next' && !memory[event.source.userId].choose_lock) {
+                return run(event.source.userId, event.replyToken)
             }
-
+        }else if (event.postback.data.startsWith('choose')){
+            const args = event.postback.data.split('.')
+            if (!memory.stage === args[1] && !memory.stage2 === args[2] && !memory.stage3 === args[3] ) return
+            const data_index = require(`./data/story/${args[1]}/index.json`)
+            const data = require(`./data/story/${args[1]}/${args[2]}.json`)
+        }
     }
 
     return Promise.resolve(null);
@@ -37,15 +42,8 @@ async function handleEvent(event//:import('@line/bot-sdk').WebhookEvent
 async function dataSetUp(userId) {
     if (!memory[userId]) memory[userId] = (await ref.child(userId).get()).val() || {}
 }
-/*
-function message(userId) {
-    const index = require(`./data/story/${memory[userId].stage}/index.json`)
-    const data = require(`./data/story/${memory[userId].stage}/${index[memory[userId].stage2]}`)
-    if (typeof(data[memory[userId].stage3]) === 'string') {
-        return { type: 'text', text: data[memory[userId].stage3] }
-    }
-}
-*/
+
+
 function run(userId, replyToken) {
     const user_data = JSON.parse(JSON.stringify(memory[userId]))
     const index = require(`./data/story/${user_data.stage}/index.json`)
@@ -59,7 +57,7 @@ function run(userId, replyToken) {
     // 純字串解析
     if (typeof data[user_data.stage3] === 'string') {
         memory[userId].stage3++
-        return client.replyMessage(replyToken, { type: 'text', text: textVar(data[user_data.stage3],user_data) })
+        return client.replyMessage(replyToken, { type: 'text', text: textVar(data[user_data.stage3], user_data) })
     }
     // need 解析
     if (data[user_data.stage3].need) {
@@ -94,24 +92,33 @@ function run(userId, replyToken) {
     }
     switch (data[user_data.stage3].type) {
         case 'text':
-            client.replyMessage(replyToken, { type: 'text', text: textVar(data[user_data.stage3].text,user_data) })
+            client.replyMessage(replyToken, { type: 'text', text: textVar(data[user_data.stage3].text, user_data) })
+            memory[userId].stage3++
             break
         case 'choose':
             {
-            let choose = []
-            for (const i of data[user_data.stage3].choose) {
-                choose.push({type:'postback',label:i['display-text'],displayText:i['send-text'],data:'ui-next'})
-            }
-            client.replyMessage(replyToken, { type: 'template', altText: altText ,template:{
-                type:'buttons',
-                text:data[user_data.stage3].text,
-                actions:choose
-            }})
+                let choose = []
+                for (const i of data[user_data.stage3].choose) {
+                    choose.push({
+                        type: 'postback', label: i['display-text'], data: `choose-${user_data.stage}-${user_data.stage2}-${user_data.stage3}`, displayText: (() => {
+                            if (i['send-text'] === undefined) return i['display-text']
+                            if (i['send-text'] === '') return null
+                            return i['send-text']
+                        })()
+                    })
+                }
+                client.replyMessage(replyToken, {
+                    type: 'template', altText: altText, template: {
+                        type: 'buttons',
+                        text: data[user_data.stage3].text,
+                        actions: choose
+                    }
+                })
+                memory[userId].choose_lock = true
             }
         default:
             break
     }
-    memory[userId].stage3++
 }
 
 function textVar(text, variable) {
