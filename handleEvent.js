5eaa5e3ba4e574e65ca9f05ed75e1538b11a34f2
Leo-768 <leo768leo768@gmail.com>
2022-06-12 22:31:28 +0800

    .

--- handleEvent.js
+++ handleEvent.js
@@ -7,7 +7,7 @@ async function handleEvent(event) {
     if (!memory.users[event.source.userId]) memory.users[event.source.userId] = (await ref.child('users').child(event.source.userId).get()).val() || {}
 
     if (event.type === 'follow') {
-        if (!memory.users[event.source.userId]) menu(event.source.userId, 'start', true)
+        if (JSON.stringify(memory.users[event.source.userId]) === '{}') menu(event.source.userId, 'start', true)
         return client.replyMessage(event.replyToken, data.messages['welcome'])
     }
 
